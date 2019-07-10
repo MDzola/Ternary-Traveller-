32b316f56b4b9a64bd787e51bb94df992d1d7e71
Michael Drew Palazola <mpalazola@hotmail.com>
2019-07-10 15:21:30 -0500

    finished project I think

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,106 +1,136 @@
-import {addNewInterest, getInterest} from "./api.js"
+import {getInterest, deleteInterest, updatedInterest} from "./api.js"
+import { createInterests } from "./createNewInterest.js";
 
-// let italySection = document.getElementById("italy")
-// let switzSection = document.getElementById("switz")
-// let franceSection = document.getElementById("france")
+let italySection = document.getElementById("italy")
+let switzSection = document.getElementById("switz")
+let franceSection = document.getElementById("france")
 
-
-
-// function listInterests() {
-
-// }
-
-// function listInterests(interest) {
-//     let elem = document.createElement("div")
-//     let li1 = document.createElement("li")
-//     let li2 = document.createElement("li")
-//     let li3 = document.createElement("li")
-//     let li4 = document.createElement("li")
-//     let div = document.createElement("div")
-//     let editBtn = document.createElement("button")
-//     let deleteBtn = document.createElement("button")
-//     elem.appendChild(li1)
-//     elem.appendChild(li2)
-//     elem.appendChild(li3)
-//     elem.appendChild(li4)
-//     elem.appendChild(div)
-//     elem.appendChild(editBtn)
-//     elem.appendChild(deleteBtn)
-
-//     editBtn.textContent = "Edit"
-//     deleteBtn.textContent = "Delete"
-
-//     li1.innerHTML = `Name of Place: ${interest.name}`
-//     li2.innerHTML = `Description: ${interest.description}`
-//     li3.innerHTML = `Cost: ${interest.cost}`
-//     li4.innerHTML =  `Review: ${interest.review}`
-
-
-// }
-
-console.log("hello")
-
-
-
-let italySection = document.getElementById("italyInterest")
-
-createAllInterests()
-
-function createAllInterests() {
-    italySection.appendChild(createNewInterest())
-}
-
-function createNewInterest() {
-   let elem = document.createElement("div")
-   let div = document.createElement("div")
-   let div2 = document.createElement("div")
-   let createBtn = document.createElement("button")
-   elem.appendChild(createBtn)
-   elem.appendChild(div)
-   elem.appendChild(div2)
-
-   div.innerHTML = `<h2>Italy</h2>`
-    div2.setAttribute("id", "italyInterest")
-
-   createBtn.textContent = "Create a New Interest"
-
-   createBtn.addEventListener("click", () => {
-       let createForm = createNewInterests()
-       addNewInterestToDOM(div2.id, createForm)
-   })
-
-    return elem
-}
+createInterests()
 
 
 
 
-function createNewInterests() {
-    return `
-    <input id="name" name="name_of_place" type="text" placeholder="name of interest">
-    <select type="text" id="country">
-        <option value="1">Italy</option>
-        <option value="2">Switzerland</option>
-        <option value="3">France</option>
-    <input id="description" name="description_of_place" type="text" placeholder="description of interest">
-    <input id="cost" name="cost_of_place" type="text" placeholder="cost of interest">
-    <input id="review" name="review_of_place" type="text" placeholder= "review of interest">
-    <button id="saveInterest-btn">Save Interest</button>
-    `
-}
+    listInterestsITALY()
+    listInterestsSWITZ()
+    listInterestsFRANCE()
 
-function addNewInterestToDOM(createContainer, createForm) {
-    document.getElementById(`${createContainer}`).innerHTML = createForm
-    document.getElementById("saveInterest-btn").addEventListener("click", () => {
-        let place = document.getElementById("country").value
-        let name  = document.getElementById("name").value
-        let description = document.getElementById("description").value
-        let cost = document.getElementById("cost").value
-        let review = document.getElementById("review").value
-        let newInterest = buildInterestObject(place, name, description, cost, review)
-        addNewInterest(newInterest)
+
+
+function listInterestsITALY() {
+    getInterest()
+    .then( interestData => {
+        interestData.forEach( interest => {
+            if (interest.placeId === 1) {
+                italySection.appendChild(createListInterests(interest))
+            }
+        })
     })
 }
+
+function listInterestsSWITZ() {
+    getInterest()
+    .then( interestData => {
+        interestData.forEach( interest => {
+            if (interest.placeId === 2) {
+                switzSection.appendChild(createListInterests(interest))
+            }
+        })
+    })
+}
+
+function listInterestsFRANCE() {
+    getInterest()
+    .then( interestData => {
+        interestData.forEach( interest => {
+            if (interest.placeId === 3) {
+                franceSection.appendChild(createListInterests(interest))
+            }
+        })
+    })
+}
+
+function createListInterests(interest) {
+    let elem = document.createElement("div")
+    let li1 = document.createElement("li")
+    let li2 = document.createElement("li")
+    let li3 = document.createElement("li")
+    let li4 = document.createElement("li")
+    let div = document.createElement("div")
+    let editBtn = document.createElement("button")
+    let deleteBtn = document.createElement("button")
+    elem.appendChild(li1)
+    elem.appendChild(li2)
+    elem.appendChild(li3)
+    elem.appendChild(li4)
+    elem.appendChild(div)
+    elem.appendChild(editBtn)
+    elem.appendChild(deleteBtn)
+
+
+    div.setAttribute("id", `editFormContainer-${interest.id}`)
+
+    editBtn.textContent = "Edit"
+    deleteBtn.textContent = "Delete"
+
+    li1.innerHTML = `Name of Place: ${interest.name}`
+    li2.innerHTML = `Description: ${interest.description}`
+    li3.innerHTML = `Cost: ${interest.cost}`
+    li4.innerHTML =  `Review: ${interest.review}`
+
+    deleteBtn.addEventListener("click", () => {
+        let choice = confirm("Do you really want to DELETE?")
+
+        if (choice === true) {
+        deleteInterest(interest.id)
+        .then( data => {
+           location.reload()
+        })
+    }
+        else{
+            location.reload()
+        }
+    })
+
+    editBtn.addEventListener("click", () => {
+        let editForm = createEditForm(interest)
+        addEditFormToDom(div.id, editForm)
+    })
+
+    return elem
+
+}
+
+
+
+    function createEditForm(interest) {
+        return `
+        <input id="interest-id" type="hidden" value=${interest.id}>
+        <input id="place-id" type="hidden" value=${interest.placeId}>
+        <div><input id="name" name="name_of_place" type="hidden" placeholder="name of interest" value =${interest.name}></div>
+        <div><input id="description" name="description_of_place" type="hidden" placeholder="description of interest" value=${interest.description}></div>
+        <div><input id="cost" name="cost_of_place" type="text" placeholder="cost of interest" value=${interest.cost}></div>
+        <div><input id="review" name="review_of_place" type="text" placeholder= "review of interest" value=${interest.review}></div>
+        <button id="save-edit-btn">Save the Edit</button>
+        `
+    }
+
+    function addEditFormToDom(editContainer, form ) {
+        document.getElementById(`${editContainer}`).innerHTML = form
+        document.getElementById("save-edit-btn").addEventListener("click", () => {
+            let interestId = document.getElementById("interest-id").value
+            let placeId = document.getElementById("place-id").value
+            let name = document.getElementById("name").value
+            let description = document.getElementById("description").value
+            let cost = document.getElementById("cost").value
+            let review = document.getElementById("review").value
+            let updateInterest = buildInterestObject(placeId, name, description, cost, review)
+            updateInterest.id = interestId
+            updatedInterest(updateInterest)
+            .then ( () => {
+                location.reload()
+            })
+        })
+    }
 
 
 function buildInterestObject(placeIdVal, nameVal, descriptionVal, costVal, reviewVal) {
